1ff457b68c85f1791566d122ce7ef5e304bdb98a
luthebao <luthebao97@gmail.com>
2023-05-23 16:46:55 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useAccount, useBalance, useToken } from 'wagmi';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [token, setToken] = useState("")
+
+    const account = useAccount()
+
+    const getToken = useToken({
+        address: token as `0x${string}`
+    })
+
+    const tokenBalance = useBalance({
+        address: account.address,
+        token: getToken.data?.address
+    })
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <main>
+                    <ConnectButton />
+                    <input placeholder='token' value={token} onChange={(e) => setToken(e.target.value)} />
+                    {
+                        getToken.data && (
+                            <div>
+                                <div>{getToken.data.name}</div>
+                            </div>
+                        )
+                    }
+                    {
+                        tokenBalance.data && (
+                            <div>
+                                {tokenBalance.data.formatted}
+                            </div>
+                        )
+                    }
+                </main>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,50 @@
+import './polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, goerli } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import App from './App';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [
+        mainnet,
+        polygon,
+        optimism,
+        arbitrum,
+        ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    ],
+    [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+    appName: 'RainbowKit demo',
+    projectId: 'YOUR_PROJECT_ID',
+    chains,
+});
+
+const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+    webSocketPublicClient,
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+            <App />
+        </RainbowKitProvider>
+    </WagmiConfig>
 );
 
 // If you want to start measuring performance in your app, pass a function
